f91db9ad335cab23094c5957b19dd8b3f60a256f
kborchers <kris.borchers@gmail.com>
2012-08-16 11:09:34 -0500

    Update banner/license to match standard license

--- grunt.js
+++ grunt.js
@@ -8,8 +8,19 @@ module.exports = function(grunt) {
             banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
                 '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
                 '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
-                '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-                ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
+                '* JBoss, Home of Professional Open Source\n' +
+                '* Copyright <%= grunt.template.today("yyyy") %>, <%= pkg.author.name %>, and individual contributors\n' +
+                '*\n' +
+                '* Licensed under the Apache License, Version 2.0 (the "License");\n' +
+                '* you may not use this file except in compliance with the License.\n' +
+                '* You may obtain a copy of the License at\n' +
+                '* <%= pkg.licenses[0].url + "\n" %>' +
+                '* Unless required by applicable law or agreed to in writing, software\n' +
+                '* distributed under the License is distributed on an "AS IS" BASIS,\n' +
+                '* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n' +
+                '* See the License for the specific language governing permissions and\n' +
+                '* limitations under the License.\n' +
+                '*/'
         },
         concat: {
             dist: {
