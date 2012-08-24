1ebf3806f628c01637615650a2a4e238d5e778e0
Kris Borchers <kris.borchers@gmail.com>
2012-08-24 08:48:08 -0500

    Fix broken link in README and fix missing syntax highlighting

--- README.md
+++ README.md
@@ -8,4 +8,4 @@ Pipeline
 
 Pipeline is a JavaScript library that provides a persistence API that is protocol agnostic and does not depend on any certain data model. Through the use of adapters, both provided and custom, user supplied, this library provides common methods like read, save and delete that will just work.
 
-See the [API docs](http://aerogear.github.com/aerogear-js/pipeline) for more info.
+See the [Pipeline API docs](http://aerogear.github.com/aerogear-js/aerogear/pipeline) for more info.
--- docs/javascripts/pdoc/item_index.js
+++ docs/javascripts/pdoc/item_index.js
@@ -1,12 +1,12 @@
 if (!window.PDoc) window.PDoc = {};
 PDoc.elements = {
-  'aerogear.pipeline.adapters': { 'name': 'aerogear.pipeline.adapters', 'type': 'namespace', 'path': 'aerogear/pipeline/adapters/index.html' },
-'aerogear.pipeline.adapters.rest#read': { 'name': 'aerogear.pipeline.adapters.rest#read', 'type': 'instance method', 'path': 'aerogear/pipeline/adapters/rest/prototype/read/index.html' },
-'aerogear.pipeline.adapters.rest#save': { 'name': 'aerogear.pipeline.adapters.rest#save', 'type': 'instance method', 'path': 'aerogear/pipeline/adapters/rest/prototype/save/index.html' },
-'aerogear.pipeline#add': { 'name': 'aerogear.pipeline#add', 'type': 'instance method', 'path': 'aerogear/pipeline/prototype/add/index.html' },
-'aerogear.pipeline': { 'name': 'aerogear.pipeline', 'type': 'namespace', 'path': 'aerogear/pipeline/index.html' },
-'aerogear.pipeline.adapters.rest': { 'name': 'aerogear.pipeline.adapters.rest', 'type': 'namespace', 'path': 'aerogear/pipeline/adapters/rest/index.html' },
+  'aerogear.pipeline.adapters.rest#read': { 'name': 'aerogear.pipeline.adapters.rest#read', 'type': 'instance method', 'path': 'aerogear/pipeline/adapters/rest/prototype/read/index.html' },
+'aerogear.pipeline.adapters.rest#remove': { 'name': 'aerogear.pipeline.adapters.rest#remove', 'type': 'instance method', 'path': 'aerogear/pipeline/adapters/rest/prototype/remove/index.html' },
 'aerogear': { 'name': 'aerogear', 'type': 'namespace', 'path': 'aerogear/index.html' },
 'aerogear.pipeline#remove': { 'name': 'aerogear.pipeline#remove', 'type': 'instance method', 'path': 'aerogear/pipeline/prototype/remove/index.html' },
-'aerogear.pipeline.adapters.rest#remove': { 'name': 'aerogear.pipeline.adapters.rest#remove', 'type': 'instance method', 'path': 'aerogear/pipeline/adapters/rest/prototype/remove/index.html' }
+'aerogear.pipeline#add': { 'name': 'aerogear.pipeline#add', 'type': 'instance method', 'path': 'aerogear/pipeline/prototype/add/index.html' },
+'aerogear.pipeline': { 'name': 'aerogear.pipeline', 'type': 'namespace', 'path': 'aerogear/pipeline/index.html' },
+'aerogear.pipeline.adapters.rest#save': { 'name': 'aerogear.pipeline.adapters.rest#save', 'type': 'instance method', 'path': 'aerogear/pipeline/adapters/rest/prototype/save/index.html' },
+'aerogear.pipeline.adapters': { 'name': 'aerogear.pipeline.adapters', 'type': 'namespace', 'path': 'aerogear/pipeline/adapters/index.html' },
+'aerogear.pipeline.adapters.rest': { 'name': 'aerogear.pipeline.adapters.rest', 'type': 'namespace', 'path': 'aerogear/pipeline/adapters/rest/index.html' }
 };
