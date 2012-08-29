8549b5e11de7c9b0a84382142722d4714425f485
Kris Borchers <kris.borchers@gmail.com>
2012-08-29 16:12:08 -0500

    Pipeline:adapter:rest - Use the isArray method now on the aerogear namespace and break the save loop once the record has been found for better performance

--- src/pipeline/adapters/rest.js
+++ src/pipeline/adapters/rest.js
@@ -1,9 +1,4 @@
 (function( aerogear, $, undefined ) {
-    // TODO: Share this across entire lib
-    function isArray( obj ) {
-        return ({}).toString.call( obj ) === "[object Array]";
-    }
-
     /**
      * aerogear.pipeline.adapters.rest
      *
@@ -98,7 +93,7 @@
                 }
 
                 var success = function( data ) {
-                    that.data = isArray( data ) ? data : [ data ];
+                    that.data = aerogear.isArray( data ) ? data : [ data ];
 
                     if ( options.ajax.success ) {
                         options.ajax.success.apply( this, arguments );
@@ -172,13 +167,14 @@
                             if ( that.data[ item ].id === data.id ) {
                                 that.data[ item ] = data;
                                 itemFound = true;
+                                break;
                             }
                         }
                         if ( !itemFound ) {
                             that.data.push( data );
                         }
                     } else {
-                        that.data = isArray( data ) ? data : [ data ];
+                        that.data = aerogear.isArray( data ) ? data : [ data ];
                     }
 
                     if ( options.ajax.success ) {
@@ -299,69 +295,6 @@
                     options.ajax || {},
                     { success: success }
                 ));
-            },
-
-            /**
-             * aerogear.pipeline.adapters.rest#filter( filterParameters[, matchAny = false] ) -> Array[Object]
-             * - filterParameters (Object): An object containing key value pairs on which to filter the pipe's data array. To filter a single parameter on multiple values, the value can be an object containing a data key with an Array of value to filter on and its own matchAny key that will override the global matchAny for that specific filter parameter.
-             * - matchAny (Boolean): When true, an item is included in the output if any of the filter parameters is matched.
-             *
-             * Returns a filtered array of data objects based on the contents of the pipe's data object and the filter parameters. This method only returns a copy of the data and leaves the original data object intact.
-             *
-             **/
-            filter: function( filterParameters, matchAny ) {
-                var filtered,
-                    i, j;
-
-                if ( !filterParameters ) {
-                    filtered = this.data || [];
-                    return filtered;
-                }
-
-                filtered = this.data.filter( function( value, index, array) {
-                    var match = matchAny ? false : true,
-                        keys = Object.keys( filterParameters ),
-                        filterObj, paramMatch, paramResult;
-
-                    for ( i = 0; i < keys.length; i++ ) {
-                        if ( filterParameters[ keys[ i ] ].data ) {
-                            // Parameter value is an object
-                            filterObj = filterParameters[ keys[ i ] ];
-                            paramResult = filterObj.matchAny ? false : true;
-
-                            for ( j = 0; j < filterObj.data.length; j++ ) {
-                                if ( filterObj.matchAny && filterObj.data[ j ] === value[ keys[ i ] ] ) {
-                                    // At least one value must match and this one does so return true
-                                    paramResult = true;
-                                    break;
-                                }
-                                if ( !filterObj.matchAny && filterObj.data[ j ] !== value[ keys[ i ] ] ) {
-                                    // All must match but this one doesn't so return false
-                                    paramResult = false;
-                                    break;
-                                }
-                            }
-                        } else {
-                            // Filter on parameter value
-                            paramResult = filterParameters[ keys[ i ] ] === value[ keys[ i ] ] ? true : false;
-                        }
-
-                        if ( matchAny && paramResult ) {
-                            // At least one item must match and this one does so return true
-                            match = true;
-                            break;
-                        }
-                        if ( !matchAny && !paramResult ) {
-                            // All must match but this one doesn't so return false
-                            match = false;
-                            break;
-                        }
-                    }
-
-                    return match;
-                });
-
-                return filtered;
             }
         };
     };
