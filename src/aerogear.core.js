0ca11258f6ed92b51aa1caa65415053d5c5ab26b
Kris Borchers <kris.borchers@gmail.com>
2012-10-05 08:45:07 -0500

    Replace valve with store

--- src/aerogear.core.js
+++ src/aerogear.core.js
@@ -9,7 +9,7 @@
          * aerogear#add( config ) -> Object
          * - config (Mixed): This can be a variety of types specifying how to create the object
          *
-         * This function is used internally by pipeline, datamanager, etc. to add a new Object (pipe, valve, etc.) to the respective collection. For specific examples look at those internal use cases.
+         * This function is used internally by pipeline, datamanager, etc. to add a new Object (pipe, store, etc.) to the respective collection. For specific examples look at those internal use cases.
          **/
         add: function ( config ) {
             var i,
@@ -46,7 +46,7 @@
          * aerogear#remove( config ) -> Object
          * - config (Mixed): This can be a variety of types specifying how to remove the object
          *
-         * This function is used internally by pipeline, datamanager, etc. to remove an Object (pipe, valve, etc.) from the respective collection. For specific examples look at those internal use cases.
+         * This function is used internally by pipeline, datamanager, etc. to remove an Object (pipe, store, etc.) from the respective collection. For specific examples look at those internal use cases.
          **/
         remove: function( config ) {
             var i,
