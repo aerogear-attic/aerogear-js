12587ad95fe6029e1fba04b1abedfd4beb481ab7
Lucas Holmquist <lholmqui@redhat.com>
2012-12-06 14:11:43 -0500

    adding plugin to handle IE9 CORS

---
+++ external/xdr.js
@@ -0,0 +1,46 @@
+ /**
+    Copyright 2012 Julian Aubourg <j@ubourg.net>
+    https://github.com/jaubourg/ajaxHooks
+    */
+( function( $, undefined) {
+    if ( window.XDomainRequest ) {
+        $.ajaxTransport(function( s ) {
+            if ( s.crossDomain && s.async ) {
+                if ( s.timeout ) {
+                    s.xdrTimeout = s.timeout;
+                    delete s.timeout;
+                }
+                var xdr;
+                return {
+                    send: function( _, complete ) {
+                        function callback( status, statusText, responses, responseHeaders ) {
+                            xdr.onload = xdr.onerror = xdr.ontimeout = $.noop;
+                            xdr = undefined;
+                            complete( status, statusText, responses, responseHeaders );
+                        }
+                        xdr = new XDomainRequest();
+                        xdr.onload = function() {
+                            callback( 200, "OK", { text: xdr.responseText }, "Content-Type: " + xdr.contentType );
+                        };
+                        xdr.onerror = function() {
+                            callback( 404, "Not Found" );
+                        };
+                        xdr.onprogress = $.noop;
+                        xdr.ontimeout = function() {
+                            callback( 0, "timeout" );
+                        };
+                        xdr.timeout = s.xdrTimeout || Number.MAX_VALUE;
+                        xdr.open( s.type, s.url );
+                        xdr.send( ( s.hasContent && s.data ) || null );
+                    },
+                    abort: function() {
+                        if ( xdr ) {
+                            xdr.onerror = $.noop;
+                            xdr.abort();
+                        }
+                    }
+                };
+            }
+        });
+    }
+})( jQuery );
