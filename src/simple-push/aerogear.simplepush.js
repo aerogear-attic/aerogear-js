2c43379d3eda34f95f91d1331ad31f566b11bcd6
Kris Borchers <kris.borchers@gmail.com>
2013-04-23 11:35:40 -0500

    SimplePush: Rework and add missing items to further match the SimplePush spec

--- src/simple-push/aerogear.simplepush.js
+++ src/simple-push/aerogear.simplepush.js
@@ -13,12 +13,12 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-(function( AeroGear, $, undefined ) {
-    var stompNotifier;
+(function( AeroGear, $, uuid, undefined ) {
+    var stompNotifier, nativePush;
     // Use browser push implementation when available
     // TODO: Test for browser-prefixed implementations
     if ( navigator.push ) {
-        return;
+        nativePush = navigator.push;
     }
 
     AeroGear.SimplePush = {};
@@ -27,98 +27,117 @@
         appInstanceID: "",
         pushNetworkLogin: "guest",
         pushNetworkPassword: "guest",
-        channelPrefix: "jms.topic.aerogear.",
+        channelPrefix: "jms.topic.aerogear",
         pushNetworkURL: "http://" + window.location.hostname + ":61614/agPushNetwork",
         pushServerURL: "http://" + window.location.hostname + ":8080/registry/device"
     };
 
-    AeroGear.SimplePush.endpoints = {};
-
-    AeroGear.SimplePush.registerWithChannel = function( name, endpoint ) {
-        // This is redundant but hopefully helpful in future proofing
-        endpoint.name = name;
-
-        AeroGear.SimplePush.endpoints[ name ] = endpoint;
-        // TODO: Inform push server?
-    };
-
     navigator.push = (function() {
-        function createChannels() {
-            // Temporarily set sessionID to true to avoid multiple inits
-            AeroGear.SimplePush.sessionID = true;
+        return {
+            register: nativePush ? nativePush.register : function() {
+                var $request, requestEvent,
+                    request = {};
 
-            // Create a Notifier connection to the Push Network
-            stompNotifier = AeroGear.Notifier({
-                name: "agPushNetwork",
-                type: "stompws",
-                settings: {
-                    connectURL: AeroGear.SimplePush.config.pushNetworkURL
+                if ( !stompNotifier || !AeroGear.SimplePush.sessionID ) {
+                    throw "SimplePushConnectionError";
                 }
-            }).clients.agPushNetwork;
 
-            stompNotifier.connect({
-                login: AeroGear.SimplePush.config.pushNetworkLogin,
-                password: AeroGear.SimplePush.config.pushNetworkPassword,
-                onConnect: function( stompFrame ) {
-                    AeroGear.SimplePush.sessionID = stompFrame.headers.session;
+                request.address = AeroGear.SimplePush.config.channelPrefix + "." + AeroGear.SimplePush.sessionID + "." + uuid();
 
-                    // Register with Unified Push server
-                    $.ajax({
-                        contentType: "application/json",
-                        dataType: "json",
-                        type: "POST",
-                        url: AeroGear.SimplePush.config.pushServerURL,
-                        headers: {
-                            "ag-push-app": AeroGear.SimplePush.config.pushAppID,
-                            "AG-Mobile-APP": AeroGear.SimplePush.config.appInstanceID
-                        },
-                        data: {
-                            token: AeroGear.SimplePush.sessionID,
-                            os: "web"
+                stompNotifier.subscribe({
+                    address: request.address,
+                    callback: function( message ) {
+                        $( navigator.push ).trigger({
+                            type: "push",
+                            message: message
+                        });
+                    }
+                });
+
+                // Provide method to inform push server
+                request.registerWithPushServer = function( messageType, endpoint ) {
+                    // TODO: Send info to push server which should cleanup / remove the setTimeout below
+                };
+
+                $request = $( request );
+                $request.on( "success", function( event ) {
+                    this.onsuccess( event );
+                });
+
+                setTimeout( function() {
+                    requestEvent = jQuery.Event( "success", {
+                        target: {
+                            result: {
+                                address: request.address
+                            }
                         }
                     });
+                    $request.trigger( requestEvent );
+                }, 100 );
 
-                    // Subscribe to personal and broadcast channels
-                    stompNotifier.subscribe([
-                        {
-                            address: AeroGear.SimplePush.config.channelPrefix + AeroGear.SimplePush.sessionID,
-                            callback: function( message ) {
-                                message.pushEndpoint = message.headers ? AeroGear.SimplePush.endpoints[ message.headers.endpoint ] : undefined;
-
-                                $( document ).trigger({
-                                    type: "push",
-                                    message: message
-                                });
-                            }
-                        },
-                        {
-                            address: AeroGear.SimplePush.config.channelPrefix + "broadcast",
-                            callback: function( message ) {
-                                message.pushEndpoint = AeroGear.SimplePush.endpoints.broadcast;
-
-                                $( document ).trigger({
-                                    type: "push",
-                                    message: message
-                                });
-                            }
-                        }
-                    ]);
-                }
-            });
-        }
-
-        return {
-            register: function() {
-                if ( !AeroGear.SimplePush.sessionID ) {
-                    createChannels();
-                }
-
-                return {};
+                return request;
             },
 
-            unregister: function( endpoint ) {
-                delete AeroGear.SimplePush.endpoints[ endpoint.name ];
+            unregister: nativePush ? nativePush.unregister : function( endpoint ) {
+                stompNotifier.unsubscribe( endpoint );
                 // TODO: Inform push server?
+            },
+
+            connect: function( pushConnectCallback ) {
+                var that = this;
+
+                if ( stompNotifier ) {
+                    return;
+                }
+
+                // Create a Notifier connection to the Push Network
+                stompNotifier = AeroGear.Notifier({
+                    name: "agPushNetwork",
+                    type: "stompws",
+                    settings: {
+                        connectURL: AeroGear.SimplePush.config.pushNetworkURL
+                    }
+                }).clients.agPushNetwork;
+
+                stompNotifier.connect({
+                    login: AeroGear.SimplePush.config.pushNetworkLogin,
+                    password: AeroGear.SimplePush.config.pushNetworkPassword,
+                    onConnect: function( stompFrame ) {
+                        var endpointID;
+                        // TODO: Replace this with a server and/or client generated UUIDv4 token
+                        AeroGear.SimplePush.sessionID = stompFrame.headers.session;
+
+                        // Register with Unified Push server
+                        $.ajax({
+                            contentType: "application/json",
+                            dataType: "json",
+                            type: "POST",
+                            url: AeroGear.SimplePush.config.pushServerURL,
+                            headers: {
+                                "ag-push-app": AeroGear.SimplePush.config.pushAppID,
+                                "AG-Mobile-APP": AeroGear.SimplePush.config.appInstanceID
+                            },
+                            data: {
+                                token: AeroGear.SimplePush.sessionID,
+                                os: "web"
+                            }
+                        });
+
+                        // Subscribe to broadcast channel
+                        stompNotifier.subscribe({
+                            address: AeroGear.SimplePush.config.channelPrefix + ".broadcast",
+                            callback: function( message ) {
+                                $( navigator.push ).trigger({
+                                    type: "push",
+                                    message: message
+                                });
+                            }
+                        });
+
+                        // Call push.connect callback
+                        pushConnectCallback.call( that, arguments );
+                    }
+                });
             }
         };
     })();
@@ -136,6 +155,6 @@
             callback.call( this, message );
         };
 
-        $( document ).on( messageType, handler );
+        $( navigator.push ).on( messageType, handler );
     };
-})( AeroGear, jQuery );
+})( AeroGear, jQuery, uuid );
