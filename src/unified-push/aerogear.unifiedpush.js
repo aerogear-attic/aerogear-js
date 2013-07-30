030e5cd4c1db10a5ae20277a03f60f51ac36a621
Matthias Wessendorf <matzew@apache.org>
2013-07-30 10:16:01 +0200

    UnifiedPush: Use metadata object as argument in registerWithPushServer, more error checking and add JSDoc

--- src/unified-push/aerogear.unifiedpush.js
+++ src/unified-push/aerogear.unifiedpush.js
@@ -15,24 +15,62 @@
 */
 (function( AeroGear, $, undefined ) {
     /**
-        DESCRIPTION
+        The UnifiedPushClient object is used to perfom register and unregister operations against the AeroGear UnifiedPush server.
         @constructs AeroGear.UnifiedPushClient
         @param {String} variantID - the id representing the mobile application variant
         @param {String} variantSecret - the secret for the mobile application variant
-        @param {String} [pushServerURL="http://" + window.location.hostname + ":8080/ag-push/rest/registry/device"] - location of the unified push server
+        @param {String} pushServerURL - the location of the UnifiedPush server
         @returns {Object} The created unified push server client
+        @example
+        //Create the UnifiedPush client object:
+        var client = AeroGear.UnifiedPushClient(
+            "myVariantID",
+            "myVariantSecret",
+            "http://SERVER:PORT/CONTEXT/rest/registry/device"
+        );
+
+        // assemble the metadata for the registration:
+        var metadata = {
+            deviceToken: "theDeviceToken",
+            alias: "some_username",
+            category: "email"
+        };
+
+        // perform the registration against the UnifiedPush server:
+        client.registerWithPushServer(metadata);
+
      */
     AeroGear.UnifiedPushClient = function( variantID, variantSecret, pushServerURL ) {
+
+        // we require all arguments to be present, otherwise it does not work
+        if ( !variantID || !variantSecret || !pushServerURL ) {
+            throw "UnifiedPushClientException";
+        }
+
         // Allow instantiation without using new
         if ( !( this instanceof AeroGear.UnifiedPushClient ) ) {
             return new AeroGear.UnifiedPushClient( variantID, variantSecret, pushServerURL );
         }
 
-        this.registerWithPushServer = function( messageType, endpoint, alias ) {
-            var RegistrationError,
-                url = pushServerURL || "http://" + window.location.hostname + ":8080/ag-push/rest/registry/device";
+        /**
+            Performs a register request against the UnifiedPush Server using the given metadata which represents a client that wants to register with the server.
+            @param {Object} metadata - the metadata for the client
+            @param {String} metadata.deviceToken - identifies the client within its PushNetwork. On Android this is the registrationID, on iOS this is the deviceToken and on SimplePush this is the channelID of the subscribed channel.
+            @param {String} [metadata.alias] - Application specific alias to identify users with the system. Common use case would be an email address or a username.
+            @param {String} [metadata.category] - In SimplePush this is the name of the registration endpoint. On Hybrid platforms like Apache Cordova this is used for tagging the registered client.
+            @param {String} [metadata.operatingSystem] - Useful on Hybrid platforms like Apache Cordova to specifiy the underlying operating system.
+            @param {String} [metadata.osVersion] - Useful on Hybrid platforms like Apache Cordova to specify the version of the underlying operating system.
+            @param {String} [metadata.deviceType] - Useful on Hybrid platforms like Apache Cordova to specify the type of the used device, like iPad or Android-Phone.
+         */
+        this.registerWithPushServer = function( metadata ) {
 
-            if ( messageType !== "broadcast" && !alias ) {
+            // we need a deviceToken, registrationID or a channelID:
+            if ( !metadata.deviceToken ) {
+                throw "UnifiedPushRegistrationException";
+            }
+
+            // if we see a category that is not the (SimplePush) broadcast, we require the alias to be present:
+            if ( metadata.category !== "broadcast" && !metadata.alias ) {
                 throw "UnifiedPushRegistrationException";
             }
 
@@ -40,32 +78,29 @@
                 contentType: "application/json",
                 dataType: "json",
                 type: "POST",
-				crossDomain: true,
-                url: url,
+                url: pushServerURL,
                 headers: {
                     "Authorization": "Basic " + window.btoa(variantID + ":" + variantSecret)
                 },
-                data: JSON.stringify({
-                    category: messageType,
-                    deviceToken: endpoint.channelID,
-                    alias: alias
-                })
+                data: JSON.stringify( metadata )
             });
         };
 
-        this.unregisterWithPushServer = function( endpoint ) {
-            var url = pushServerURL || "http://" + window.location.hostname + ":8080/ag-push/rest/registry/device";
+        /**
+            Performs an unregister request against the UnifiedPush Server for the given deviceToken. The deviceToken identifies the client within its PushNetwork. On Android this is the registrationID, on iOS this is the deviceToken and on SimplePush this is the channelID of the subscribed channel.
+            @param {String} deviceToken - unique String which identifies the client that is being unregistered.
+         */
+        this.unregisterWithPushServer = function( deviceToken ) {
             $.ajax({
                 contentType: "application/json",
                 dataType: "json",
                 type: "DELETE",
-                crossDomain: true,
-                url: url + "/" + endpoint.channelID,
+                url: pushServerURL + "/" + deviceToken,
                 headers: {
                     "Authorization": "Basic " + window.btoa(variantID + ":" + variantSecret)
                 },
                 data: JSON.stringify({
-                    deviceToken: endpoint.channelID
+                    deviceToken: deviceToken
                 })
             });
         };
