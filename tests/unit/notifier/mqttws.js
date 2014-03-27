c65b6bbe1a6c464678c23fc120898ff1cfe46652
tolis-e <tolisemm@yahoo.gr>
2014-03-27 20:11:07 +0200

    Add basic unit tests for MQTT notifier

---
+++ tests/unit/notifier/mqttws.js
@@ -0,0 +1,63 @@
+(function( $ ) {
+
+module( "Notifier: MQTT Websocket" );
+
+test( "create - object", function() {
+    expect( 2 );
+
+    var mqtt = AeroGear.Notifier({
+        name: "createTest",
+        type: "mqttws"
+    }).clients;
+
+    equal( Object.keys( mqtt ).length, 1, "1 Client created" );
+    equal( Object.keys( mqtt )[ 0 ], "createTest", "Client Name createTest" );
+});
+
+test( "create - array", function() {
+    expect( 3 );
+
+    var mqtt = AeroGear.Notifier([
+    {
+        name: "createTest",
+        type: "mqttws"
+    },
+    {
+        name: "createTest2",
+        type: "mqttws"
+    }]).clients;
+
+    equal( Object.keys( mqtt ).length, 2, "2 Clients created" );
+    equal( Object.keys( mqtt )[ 0 ], "createTest", "Client Name createTest" );
+    equal( Object.keys( mqtt )[ 1 ], "createTest2", "Client Name createTest2" );
+});
+
+// Add client test
+test( "add method", function() {
+    expect( 2 );
+
+    var mqtt = AeroGear.Notifier().add({
+        name: "addTest",
+        type: "mqttws"
+    }).clients;
+
+    equal( Object.keys( mqtt ).length, 1, "Single Client added" );
+    equal( Object.keys( mqtt )[ 0 ], "addTest", "Client Name addTest" );
+});
+
+// Remove client test
+test( "remove method", function() {
+    expect( 3 );
+
+    var mqtt = AeroGear.Notifier({
+        name: "removeTest",
+        type: "mqttws"
+    });
+
+    equal( Object.keys( mqtt.clients ).length, 1, "Single Client added" );
+    mqtt.remove("removeTest");
+    equal( Object.keys( mqtt.clients ).length, 0, "Single Client removed" );
+    equal( mqtt.clients.removeTest, undefined, "Removed client is really gone" );
+});
+
+})( jQuery );
