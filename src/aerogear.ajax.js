68a99e5782e6ca561dc6a1e2076299a9bcfef7f2
tolis-e <tolisemm@yahoo.gr>
2014-06-08 12:14:33 +0300

    AGJS-186 aeroGear.ajax - support for query params

--- src/aerogear.ajax.js
+++ src/aerogear.ajax.js
@@ -20,24 +20,61 @@ AeroGear.ajax = function( settings ) {
 
         var request = new XMLHttpRequest(),
             that = this,
+            requestType = ( settings.type || "GET" ).toUpperCase(),
+            responseType = ( settings.dataType || "json" ).toLowerCase(),
+            accept = ( settings.accept || "application/json" ).toLowerCase(),
+            // TODO: compare contentType by checking if it starts with some value since it might contains the charset as well
+            contentType = ( settings.contentType || "application/json" ).toLowerCase(),
             _oncomplete,
-            header;
+            header,
+            name,
+            urlEncodedData = [],
+            url = settings.url,
+            data = settings.data;
 
-        request.open( settings.type || "GET", settings.url, true, settings.username, settings.password );
+        if ( settings.params ) {
+            // encode params
+            if( requestType === "GET" || ( requestType === "POST" && contentType === "application/x-www-form-urlencoded" ) ) {
+                for( name in settings.params ) {
+                    urlEncodedData.push( encodeURIComponent( name ) + "=" + encodeURIComponent( settings.params[ name ] || "" ) );
+                }
+            // add params in request body
+            } else if ( contentType === "application/json" ) {
+                data = data || {};
+                data.params = data.params || {};
+                AeroGear.extend( data.params,  settings.params );
+            }
+        }
 
-        request.responseType = "json";
-        request.setRequestHeader( "Content-Type", "application/json" );
-        request.setRequestHeader( "Accept", "application/json" );
+        if ( contentType === "application/x-www-form-urlencoded" ) {
+            if ( data ) {
+                for( name in data ) {
+                    urlEncodedData.push( encodeURIComponent( name ) + '=' + encodeURIComponent( data[ name ] ) );
+                }
+            }
+            data = urlEncodedData.join( "&" );
+        }
+
+        // if is GET request & URL params exist then add them in URL
+        if( requestType === "GET" && urlEncodedData.length > 0 ) {
+            url += "?" + urlEncodedData.join( "&" );
+        }
+
+        request.open( requestType, url, true, settings.username, settings.password );
+
+        request.responseType = responseType;
+        request.setRequestHeader( "Content-Type", contentType );
+        request.setRequestHeader( "Accept", accept );
 
         if( settings.headers ) {
-            for( header in settings.headers ) {
+            for ( header in settings.headers ) {
                 request.setRequestHeader( header, settings.headers[ header ] );
             }
         }
 
         // Success and 400's
         request.onload = function() {
-            var status =( request.status < 400 ) ? "success" : "error";
+            var status = ( request.status < 400 ) ? "success" : "error";
 
             if( status === "success" ) {
                 resolve( request );
@@ -64,6 +101,6 @@ AeroGear.ajax = function( settings ) {
             }
         };
 
-        request.send( settings.data );
+        request.send( data );
     });
 };
