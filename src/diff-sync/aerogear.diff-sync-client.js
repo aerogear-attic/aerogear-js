c193b08f40ff4b19bcd2ea4e836513a5cb0a2898
Lucas Holmquist <lholmqui@redhat.com>
2014-11-11 15:19:18 -0500

    Initial Addition of Sync Client/Engine for Diff Sync
    This method combines the 'diff' and 'sendEdits' methods.  This makes it easier for the end user to not have to worry about doing both.

---
+++ src/diff-sync/aerogear.diff-sync-client.js
@@ -0,0 +1,230 @@
+/* AeroGear JavaScript Library
+* https://github.com/aerogear/aerogear-js
+* JBoss, Home of Professional Open Source
+* Copyright Red Hat, Inc., and individual contributors
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+* http://www.apache.org/licenses/LICENSE-2.0
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+/**
+    The AeroGear Differential Sync Client.
+    @status Experimental
+    @constructs AeroGear.UnifiedPushClient
+    @param {Object} config - A configuration
+    @param {String} config.serverUrl - the url of the Differential Sync Server
+    @param {Object} [config.syncEngine="AeroGear.DiffSyncEngine"] -
+    @param {function} [config.onopen] -
+    @param {function} [config.onclose] -
+    @param {function} [config.onsync] -
+    @param {function} [config.onerror] -
+    @returns {object} diffSyncClient - The created DiffSyncClient
+    @example
+ */
+(function( AeroGear, undefined) {
+    AeroGear.DiffSyncClient = function ( config ) {
+        if ( ! ( this instanceof AeroGear.DiffSyncClient ) ) {
+            return new AeroGear.DiffSyncClient( config );
+        }
+
+        config = config || {};
+
+        var ws,
+            sendQueue = [],
+            that = this,
+            syncEngine = config.syncEngine || new AeroGear.DiffSyncEngine({name: 'jsonPatchEngine'}).engine.jsonPatchEngine;
+
+        if ( config.serverUrl === undefined ) {
+            throw new Error( "'config.serverUrl' must be specified" );
+        }
+
+        /**
+            Connects to the Differential Sync Server using WebSockets
+        */
+        this.connect = function() {
+            ws = new WebSocket( config.serverUrl );
+            ws.onopen = function ( e ) {
+                if ( config.onopen ) {
+                    config.onopen.apply( this, arguments );
+                }
+
+                //console.log ( 'WebSocket opened. SendQueue.length=', sendQueue.length );
+                while ( sendQueue.length ) {
+                    var task = sendQueue.pop();
+                    if ( task.type === "add" ) {
+                        send ( task.type, task.msg );
+                    } else {
+                        that.sendEdits( task.msg );
+                    }
+                }
+            };
+            ws.onmessage = function( e ) {
+                var data, doc;
+
+                try {
+                    data = JSON.parse( e.data );
+                } catch( err ) {
+                    data = {};
+                }
+
+                if ( data ) {
+                    that._patch( data );
+                }
+
+                doc = that.getDocument( data.id );
+
+                if( config.onsync ) {
+                    config.onsync.call( this, doc, e );
+                }
+            };
+            ws.onerror = function( e ) {
+                if ( config.onerror ) {
+                    config.onerror.apply( this, arguments );
+                }
+            };
+            ws.onclose = function( e ) {
+                if ( config.onclose ) {
+                     config.onclose.apply( this, arguments);
+                }
+            };
+        };
+
+        // connect needs to be callable for implementing reconnect.
+        this.connect();
+
+        /**
+            Disconnects from the Differential Sync Server closing it's Websocket connection
+        */
+        this.disconnect = function() {
+            //console.log('Closing Connection');
+            ws.close();
+        };
+
+        /**
+            patch -
+            @param {Object} data - The data to be patched
+        */
+        this._patch = function( data ) {
+            syncEngine.patch( data );
+        };
+
+        /**
+            getDocument -
+            @param {String} id - the id of the document to get
+            @returns {Object} - The document from the sync engine
+        */
+        this.getDocument = function( id ) {
+            return syncEngine.getDocument( id );
+        };
+
+        /**
+            diff
+            @param {Object} data - the data to perform a diff on
+            @returns {Object} - An Object containing the edits from the Sync Engine
+        */
+        this._diff = function( data ) {
+            return syncEngine.diff( data );
+        };
+
+        /**
+            addDocument
+            @param {Object} doc - a document to add to the sync engine
+        */
+        this.addDocument = function( doc ) {
+            syncEngine.addDocument( doc );
+
+            if ( ws.readyState === 0 ) {
+                sendQueue.push( { type: "add", msg: doc } );
+            } else if ( ws.readyState === 1 ) {
+                send( "add", doc );
+            }
+        };
+
+        /**
+            sendEdits
+            @param {Object} edit - the edits to be sent to the server
+        */
+        this._sendEdits = function( edit ) {
+            if ( ws.readyState === WebSocket.OPEN ) {
+                //console.log( 'sending edits:', edit );
+                ws.send( JSON.stringify( edit ) );
+            } else {
+                //console.log("Client is not connected. Add edit to queue");
+                if ( sendQueue.length === 0 ) {
+                    sendQueue.push( { type: "patch", msg: edit } );
+                } else {
+                    var updated = false;
+                    for (var i = 0 ; i < sendQueue.length; i++ ) {
+                        var task = sendQueue[i];
+                        if (task.type === "patch" && task.msg.clientId === edit.clientId && task.msg.id === edit.id) {
+                            for (var j = 0 ; j < edit.edits.length; j++) {
+                                task.msg.edits.push( edit.edits[j] );
+                            }
+                            updated = true;
+                        }
+                    }
+                    if ( !updated ) {
+                        sendQueue.push( { type: "patch", msg: edit } );
+                    }
+                }
+            }
+        };
+
+        /**
+            sync
+            @param {Object} data - the Data to be sync'd with the server
+        */
+        this.sync = function( data ) {
+            var edits = that._diff( data );
+            that._sendEdits( edits );
+        };
+
+        /**
+            removeDoc
+            TODO
+        */
+        this.removeDoc = function( doc ) {
+           // TODO?
+           // console.log( "removing  doc from engine" );
+        };
+
+        /**
+            fetch
+            @param {String} docId - the id of a document to fetch from the Server
+        */
+        this.fetch = function( docId ) {
+            var doc, edits, task;
+
+            if ( sendQueue.length === 0 ) {
+                doc = syncEngine.getDocument( docId );
+                that.sync( doc );
+            } else {
+                while ( sendQueue.length ) {
+                    task = sendQueue.shift();
+                    if ( task.type === "add" ) {
+                        send ( task.type, task.msg );
+                    } else {
+                        that._sendEdits( task.msg );
+                    }
+                }
+            }
+        };
+
+        /**
+            send
+            @param {String} msgType
+            @param {Object} doc
+        */
+        var send = function ( msgType, doc ) {
+            var json = { msgType: msgType, id: doc.id, clientId: doc.clientId, content: doc.content };
+            //console.log ( 'sending ' + JSON.stringify ( json ) );
+            ws.send( JSON.stringify ( json ) );
+        };
+    };
+})( AeroGear );
---
+++ tests/unit/sync/diff-sync-engine-json-patch.js
@@ -0,0 +1,186 @@
+(function() {
+
+    module( 'JSON Patch - Sync Engine test' );
+
+    test ( 'JSON Patch should be the default engine', function() {
+        var engine = AeroGear.DiffSyncEngine({name: 'thing' }).engines.thing;
+        equal( engine instanceof AeroGear.DiffSyncEngine.adapters.jsonPatch , true, 'Should be an instance of jsonpatch adapter' );
+    });
+
+    test( 'add document', function() {
+        var engine = AeroGear.DiffSyncEngine({name: 'thing'}).engines.thing, doc = { id: 1234, clientId: 'client1', content: { name: 'Fletch' } };
+        engine.addDocument( { id: 1234, clientId: 'client1', content: { name: 'Fletch' } } );
+        var actualDoc = engine.getDocument( 1234 );
+        equal( actualDoc.id, 1234, 'Document id should match' );
+    });
+
+    test( 'diff document', function() {
+        var engine = AeroGear.DiffSyncEngine({name: 'thing'}).engines.thing;
+        var doc = { id: 1234, clientId: 'client1', content: { name: 'Fletch' } };
+        engine.addDocument( doc );
+
+        // update the name field
+        doc.content.name = 'Mr.Poon';
+
+        var patchMsg = engine.diff( doc );
+        equal ( patchMsg.msgType, 'patch', 'The message type should be "patch"');
+        equal ( patchMsg.id, 1234, 'document id should be 1234');
+        equal ( patchMsg.clientId, 'client1', 'clientId should be client1');
+
+        var edit = patchMsg.edits[0];
+        equal ( edit.clientVersion, 0, 'version should be zero');
+        equal ( edit.serverVersion, 0, 'version should be zero');
+        equal ( edit.checksum, '', 'checksum is currently not implemented.');
+
+        var diffs = edit.diffs[0];
+        ok( diffs instanceof Array, 'diffs should be an array of tuples' );
+        ok( diffs.length === 1, 'there should be 1 diff tuples generated');
+        //{op: "replace", path: "/name", value: "Mr.Poon"}
+        equal ( diffs[0].op, 'replace', 'operation should be replace');
+        equal ( diffs[0].value, 'Mr.Poon','value should be "Mr. Poon"');
+        equal ( diffs[0].path, '/name','path should be /name');
+    });
+
+    test( 'patch document', function() {
+        var engine = AeroGear.DiffSyncEngine({name: 'thing'}).engines.thing;
+        var doc = { id: 1234, clientId: 'client1', content: {name: 'Fletch' } };
+        engine.addDocument( doc );
+
+        var shadowDoc = engine.getShadow( doc.id );
+
+        shadowDoc.content.name = 'Mr.Poon';
+
+        var patch = engine.patchDocument( shadowDoc );
+        equal( patch, true, 'patch should have been successful.' );
+
+        doc = engine.getDocument( doc.id );
+        equal( doc.content.name, 'Mr.Poon', 'name should be updated');
+    });
+
+    test( 'patch shadow - content is a String', function() {
+        var engine = AeroGear.DiffSyncEngine({name: 'thing'}).engines.thing;
+        var content = 'Fletch';
+        var doc = { id: 1234, clientId: 'client1', content: content };
+        var shadow;
+        engine.addDocument( doc );
+        doc.content = 'John Coctolstol';
+
+        shadow = engine.getShadow( doc.id );
+
+        console.log(shadow);
+
+        var patchMsg = {
+            msgType: 'patch',
+            id: doc.id,
+            clientId: shadow.clientId,
+            edits: [{
+                clientVersion: shadow.clientVersion,
+                serverVersion: shadow.serverVersion,
+                // currently not implemented but we probably need this for checking the client and server shadow are identical be for patching.
+                checksum: '',
+                diffs: [jsonpatch.compare(shadow.content, doc.content)]
+            }]
+        };
+        //var patchMsg = engine.diff( doc );
+        console.log('patchMsg', patchMsg);
+
+        var updatedShadow = engine.patchShadow( patchMsg );
+        console.log(updatedShadow);
+        equal( updatedShadow.content, 'John Coctolstol', 'name should have been updated to John Coctolstol' );
+        equal( shadow.serverVersion, 1, 'Server version should have been updated.' );
+        equal( shadow.clientVersion, 0, 'Client version should not have been updated.' );
+    });
+
+    test( 'patch shadow - content is an Object', function() {
+        var engine = AeroGear.DiffSyncEngine({name: 'thing'}).engines.thing;
+        var content = { name: 'Fletch' };
+        var doc = { id: 1234, clientId: 'client1', content: content };
+        var shadow;
+        engine.addDocument( doc );
+        doc.content.name = 'John Coctolstol';
+
+        shadow = engine.getShadow( doc.id );
+
+        var patchMsg = {
+            msgType: 'patch',
+            id: doc.id,
+            clientId: shadow.clientId,
+            edits: [{
+                clientVersion: shadow.clientVersion,
+                serverVersion: shadow.serverVersion,
+                // currently not implemented but we probably need this for checking the client and server shadow are identical be for patching.
+                checksum: '',
+                diffs: [jsonpatch.compare(shadow.content, doc.content)]
+            }]
+        };
+        //var patchMsg = engine.diff( doc );
+        console.log('patchMsg', patchMsg);
+
+        var updatedShadow = engine.patchShadow( patchMsg );
+        equal( JSON.stringify(updatedShadow.content), '{"name":"John Coctolstol"}', 'name should have been updated to John Coctolstol' );
+        equal( shadow.serverVersion, 1, 'Server version should have been updated.' );
+        equal( shadow.clientVersion, 0, 'Client version should not have been updated.' );
+    });
+
+    test( 'already seen edit should be deleted', function() {
+        var engine = AeroGear.DiffSyncEngine({name: 'thing'}).engines.thing;
+        var content = { name: 'Fletch' };
+        var doc = { id: 1234, clientId: 'client1', content: content };
+        var shadow;
+        engine.addDocument( doc );
+        doc.content.name = 'John Coctolstol';
+
+        shadow = engine.getShadow( doc.id );
+        var patchMsg = {
+            msgType: 'patch',
+            id: doc.id,
+            clientId: shadow.clientId,
+            edits: [{
+                clientVersion: shadow.clientVersion,
+                serverVersion: shadow.serverVersion,
+                checksum: '',
+                diffs: [jsonpatch.compare(shadow.content, doc.content)]
+            }]
+        };
+
+        // patch twice, second patch should not change the outcome and should simply be discarded.
+        engine.patchShadow( patchMsg );
+        var updatedShadow = engine.patchShadow( patchMsg );
+
+        equal( JSON.stringify(updatedShadow.content), '{"name":"John Coctolstol"}', 'name should have been updated to John Coctolstol' );
+        equal( shadow.serverVersion, 1, 'Server version should have been updated.' );
+        equal( shadow.clientVersion, 0, 'Client version should not have been updated.' );
+    });
+
+    test( 'restore from backup', function() {
+        var engine = AeroGear.DiffSyncEngine({name: 'thing'}).engines.thing;
+        var content = { name: 'Fletch' };
+        var doc = { id: 1234, clientId: 'client1', content: content };
+        var shadow;
+        engine.addDocument( doc );
+        doc.content.name = 'John Coctolstol';
+
+        shadow = engine.getShadow( doc.id );
+        var patchMsg = {
+            msgType: 'patch',
+            id: doc.id,
+            clientId: shadow.clientId,
+            edits: [{
+                clientVersion: 0,
+                serverVersion: 0,
+                checksum: '',
+                diffs: [jsonpatch.compare(shadow.content, doc.content)]
+            }]
+        };
+
+        // simulate that the client has performed a diff which will increment the client version.
+        shadow.clientVersion = 1;
+        engine._saveShadow( shadow );
+
+        var updatedShadow = engine.patchShadow( patchMsg );
+
+        equal( JSON.stringify(updatedShadow.content), '{"name":"John Coctolstol"}', 'name should have been updated to John Coctolstol' );
+        equal( shadow.serverVersion, 0, 'Server version should have been updated.' );
+        equal( shadow.clientVersion, 1, 'Client version should not have been updated.' );
+    });
+})();
